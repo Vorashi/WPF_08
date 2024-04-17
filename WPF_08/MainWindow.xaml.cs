2edd14dfea76b025db34f0471645c439a9274528
Матвей Шаров <matvejsarov673@gmail.com>
2024-04-17 23:01:35 +0300

    Добавьте файлы проекта.

---
+++ WPF_08/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_08
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void RollMin(object sender, RoutedEventArgs e) => Window.WindowState = WindowState.Minimized;
+
+        public void ExpandTerritory(object sender, RoutedEventArgs e) => Window.WindowState = WindowState.Maximized;
+
+        public void Recovery(object sender, RoutedEventArgs e) => Window.WindowState= WindowState.Normal;
+
+        public void Close(object sender, RoutedEventArgs e) => Close();
+    }
+}
